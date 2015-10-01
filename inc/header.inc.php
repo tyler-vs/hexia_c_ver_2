80161700db35d9e3b32c12139b9cd80cf88c275d
tyler-vs <tylervucf@knights.ucf.edu>
2015-09-30 23:09:28 -0400

    predeploy ready, all files working locally CHECKED, this commit is right before deploying to the sulley server.

---
+++ inc/header.inc.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  
+  <meta charset="utf-8">
+
+  <title>Hexia_C</title>
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+
+  <link rel='stylesheet prefetch' href='http://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css'>
+  <link rel='stylesheet prefetch' href='http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css'>
+
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+
+<body>
+
+  <!-- Header Navigation
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <header class="header" role="banner">
+    <div class="container">
+      <!-- logo -->
+      <div class="logo">
+        <a href="http://demo.html5blank.com">
+          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/307033/hexia-logo.png" alt="Logo" class="logo-img">
+        </a>
+      </div>
+      <!-- /logo -->
+      
+      <!-- nav -->
+      <nav class="nav" role="navigation">
+        <div class="menu">
+          <ul>
+            <li class="page_item"><a href="">Home</a></li>
+            <li class="page_item"><a href="">Catalog</a></li>
+            <li class="page_item"><a href="">Featured</a></li>
+            <li class="page_item"><a href="">Shopping Cart</a></li>
+            <li class="page_item"><a href="">Fullfillment</a></li>
+            <li class="page_item"><a href="">User Profile</a></li>
+            <li class="page_item"><a href="">Search</a></li>
+          </ul>
+        </div>
+      </nav>
+      <!-- /nav -->
+      <div id="top-search">
+        <a href="#">Sign-in</a>
+        <a href="#">Register</a>
+        <form class="top-search_box">
+          <input type="text" placeholder="search" />
+          <button class="button-primary"><i class="fa fa-search"></i> Search</button>
+        </form>
+        <button href="shopping_cart.php"><i class="fa fa-shopping-cart"></i> Shopping Cart</button>
+      </div>
+      </div>
+  </header>
+  <!-- /header -->
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php  
+
+/*
+*   index / home.php
+*
+*
+*
+* 
+ */
+
+include 'inc/header.inc.php';
+?>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  
+  <section class="section">
+    <div class="container">
+      <div class="row">
+        <h2 class="section-title">Section title</h2>
+      </div>
+      <div class="row">
+        <div class="twelve columns">
+          <p>
+            <strong>Welcome To Hexia!</strong><br>
+            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam voluptas unde cum sunt esse minus sequi quidem earum, quo nulla autem nobis! Quisquam odio, hic, dolore possimus adipisci dolorum illum.</span>
+            <span>Praesentium optio facere, quasi excepturi commodi assumenda rerum earum explicabo accusantium eum laborum! Ut numquam, enim doloremque libero vero illo voluptatibus odit laudantium error, autem facilis sint. Earum, iste, dicta?</span>
+            <span>Saepe praesentium velit ipsa aliquid ducimus harum consectetur esse nihil, recusandae doloremque dolorum rem aliquam voluptate obcaecati, quam laboriosam quibusdam quia laborum. Quasi voluptatum harum eius alias distinctio aliquid animi.</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+<?php
+/*
+* grab footer
+* 
+ */
+
+include 'inc/footer.inc.php';
